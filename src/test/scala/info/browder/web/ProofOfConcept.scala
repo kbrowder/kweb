20537e15657c1d4f7a218213a30a722c484c511a
kbrowder <kbrowder@gmail.com>
2013-03-17 18:20:11 -0400

    use a template

--- src/test/scala/info/browder/web/ProofOfConcept.scala
+++ src/test/scala/info/browder/web/ProofOfConcept.scala
@@ -1,12 +1,10 @@
-package net.kbserve.kblog
+package info.browder.web
 
 import scala.collection.immutable.Map
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
-
 import org.scalatest.FeatureSpec
 import org.scalatest.matchers.ShouldMatchers
-
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.actor.actorRef2Scala
@@ -16,28 +14,26 @@ import akka.camel.Consumer
 import akka.camel.Producer
 import akka.pattern.ask
 import akka.util.Timeout
+import org.apache.camel.Exchange
+import akka.actor.ActorRef
 
-class BasicEndpoint extends Consumer {
-  implicit val timeout = Timeout(25 seconds)
+class BasicEndpoint(transformer: ActorRef) extends Consumer {
+  implicit val timeout = Timeout(5 seconds)
   def endpointUri = "jetty:http://localhost:8888/example?matchOnUriPrefix=true"
   def receive = {
-    case msg: CamelMessage => {
-      sender ! ("<body><head><title>hi</title></head></body>")
+    case msg => {
+      //println(msg)
+      transformer forward msg
+      //sender ! ("<body><head><title>hi</title></head></body>")
+
     }
   }
 }
 
-class Basic2Endpoint extends Consumer {
-  implicit val timeout = Timeout(25 seconds)
-  def endpointUri = "jetty:http://localhost:8888/endpoint2"
-  def receive = {
-    case msg: CamelMessage => {
-      sender ! ("<body><head><title>h2</title></head></body>")
-    }
-  }
+class ScalateTransform extends Producer {
+  def endpointUri = "scalate:" + this.getClass().getResource("template.jade")
+  
 }
-
-
 
 class BasicProducer extends Producer {
   def endpointUri = "http://localhost:8888/example/bar"
@@ -48,23 +44,32 @@ class ProofOfConcept extends FeatureSpec with ShouldMatchers {
     scenario("routebox works")(pending)
     scenario("http works") {
       implicit val testSystem = ActorSystem("testsystem")
-      val endpoint = testSystem.actorOf(Props[BasicEndpoint])
+      this.getClass().getResource("template.jade") should not be (null)
+
+      val transformer = testSystem.actorOf(Props[ScalateTransform])
+      val endpoint = testSystem.actorOf(Props(new BasicEndpoint(transformer)))
       val producer = testSystem.actorOf(Props[BasicProducer])
 
-      implicit val timeout = Timeout(25 seconds)
+      implicit val timeout = Timeout(5 seconds)
       import testSystem.dispatcher
 
       val message = new CamelMessage("Hello World", Map[String, Any]())
-      
-      val res = (producer ? "hi" ).mapTo[CamelMessage]
+
+      val res = (producer ? "hi").mapTo[CamelMessage]
       Await.result(res, timeout.duration)
-      println("actualRes")
-      
+
       res.value should not be (None)
-      res.value.get should be ('success)
+      res.value.get should be('success)
       val returnedMessage = res.value.get.get
+      
+      implicit val camelContext = CamelExtension(testSystem).context
+      
       returnedMessage.headers should contain key ("CamelHttpResponseCode")
-      returnedMessage.headers.get("CamelHttpResponseCode") should be (Some(200))
+      returnedMessage.headers.get("CamelHttpResponseCode") should be(Some(200))
+      
+      println(returnedMessage.bodyAs[String])
+      //val xmlRes = xml.XML.load(returnedMessage.bodyAs[String])
+      //(xmlRes \\ "title").text should be ("hi world")
       
 
     }
